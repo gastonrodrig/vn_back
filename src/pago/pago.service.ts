c53b710c08285c36c9d3c18b64d0a9db04f81c45
gastonrodrig04 <202111913@urp.edu.pe>
2024-09-13 10:24:06 -0500

    pago updated

--- src/pago/pago.service.ts
+++ src/pago/pago.service.ts
@@ -1,4 +1,3 @@
-// src/pago/pago.service.ts
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -13,4 +12,8 @@ export class PagoService {
     const createdPago = new this.pagoModel(createPagoDto);
     return createdPago.save();
   }
+
+  async findAll() {
+    return this.pagoModel.find().exec();
+  }
 }
