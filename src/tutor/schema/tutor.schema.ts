54fd5283da0ffa0424eb297020df2731a40bfbd4
Gaston <gaston.rodriguez@icloud.com>
2024-09-28 22:22:25 -0500

    backend tutor finished

--- src/tutor/schema/tutor.schema.ts
+++ src/tutor/schema/tutor.schema.ts
@@ -4,38 +4,38 @@ import { Multimedia } from 'src/multimedia/schema/multimedia.schema';
 
 @Schema({ collection: 'Tutor'})
 export class Tutor {
-   @Prop()
-   nombre: string;
-   
-   @Prop()
-   apellido: string;
+  @Prop()
+  nombre: string;
+  
+  @Prop()
+  apellido: string;
 
-   @Prop()
-   direccion: string;
+  @Prop()
+  direccion: string;
 
-   @Prop()
-   telefono: string;
+  @Prop()
+  telefono: string;
 
-   @Prop()
-   numero_documento: string;
+  @Prop()
+  numero_documento: string;
 
-   @Prop({ type: Types.ObjectId, ref: 'Documento'})
-   documento: Types.ObjectId;
+  @Prop({ type: Types.ObjectId, ref: 'Documento'})
+  documento: Types.ObjectId;
 
-   @Prop({ type: Types.ObjectId, ref: 'Multimedia'})
-   multimedia: Types.ObjectId | Multimedia;
+  @Prop({ type: Types.ObjectId, ref: 'Multimedia'})
+  multimedia: Types.ObjectId | Multimedia;
 
-   @Prop({ type: Types.ObjectId, ref: 'User' })
-   user: Types.ObjectId;
+  @Prop({ type: Types.ObjectId, ref: 'User' })
+  user: Types.ObjectId;
 
-   @Prop({ type: Types.ObjectId, ref: 'Seccion'})
-   seccion: Types.ObjectId;
+  @Prop({ type: Types.ObjectId, ref: 'Seccion'})
+  seccion: Types.ObjectId;
 
-   @Prop({ type: Types.ObjectId, ref: 'Grado' })
-   grado: Types.ObjectId;
+  @Prop({ type: Types.ObjectId, ref: 'Grado' })
+  grado: Types.ObjectId;
 
-   @Prop({ type: Types.ObjectId, ref: 'PeriodoEscolar' })
-   periodo: Types.ObjectId;
+  @Prop({ type: Types.ObjectId, ref: 'PeriodoEscolar' })
+  periodo: Types.ObjectId;
 }
 
 export const TutorSchema = SchemaFactory.createForClass(Tutor)
